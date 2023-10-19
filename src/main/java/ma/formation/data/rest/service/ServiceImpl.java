43173546c4a11d176d739d3b72b771ffab76a8b8
abbouformations <135717843+abbouformations@users.noreply.github.com>
2023-10-19 16:43:41 +0100

    Update ServiceImpl.java

--- src/main/java/ma/formation/data/rest/service/ServiceImpl.java
+++ src/main/java/ma/formation/data/rest/service/ServiceImpl.java
@@ -1,16 +1,16 @@
 package ma.formation.data.rest.service;
 
+import lombok.AllArgsConstructor;
 import ma.formation.data.rest.dao.CategorieRepository;
 import ma.formation.data.rest.service.model.Article;
 import ma.formation.data.rest.service.model.Categorie;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class ServiceImpl implements IService {
-    @Autowired
     private CategorieRepository categorieRepository;
 
     @Override
